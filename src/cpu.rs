21a5f7e0bcdd22f58833cf8fd7504d75a7beb574
raye <174672189+SkyStrikerRaye@users.noreply.github.com>
2024-12-27 14:26:25 +0800

    add CPU struct with initialization method

---
+++ src/cpu.rs
@@ -0,0 +1,16 @@
+pub struct CPU {
+    pub register_a : u8,
+    pub status : u8,
+    pub program_counter : u16,
+}
+
+
+impl CPU {
+    pub fn new() -> CPU {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+}
